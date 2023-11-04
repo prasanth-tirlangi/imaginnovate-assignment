a920928c9029b266c69431d1cd633f1327dd57cf
prasanth-tirlangi <prasanth.tirlangi45@gmail.com>
2023-11-04 10:56:31 +0530

    Initial commit

---
+++ src/main/java/com/prasanth/assignment/ImaginnovateAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.prasanth.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaginnovateAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaginnovateAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prasanth/assignment/ImaginnovateAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasanth.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImaginnovateAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
